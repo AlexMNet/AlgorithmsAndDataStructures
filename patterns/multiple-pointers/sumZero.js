473a77fad961e8b97026874946698222b2e3516f
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-03-02 19:31:39 -0800

    add multiple pointers.

--- patterns/multiple-pointers/sumZero.js
+++ patterns/multiple-pointers/sumZero.js
@@ -4,9 +4,34 @@ The function should find the first pair where the sum is 0
 
 
 sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
+sumZero([-4, -2, -1, 0, 1, 2, 3]) // [-2, 2]
 sumZero([-2, 0, 1, 3]) //undefined
 sumZero([1, 2, 3]) //undefined
 */
+
+//Solution #1
+const sumZero = (arr) => {
+  if (arr.length === 0) {
+    throw new Error('Array must have a length!');
+  }
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low !== high) {
+    if (arr[low] + arr[high] === 0) {
+      return [arr[low], arr[high]];
+    }
+
+    if (arr[low] + arr[high] < 0) {
+      low++;
+    }
+
+    if (arr[low] + arr[high] > 0) {
+      high--;
+    }
+  }
+  return undefined;
+};
 
 //Naive solution
 const sumZero = (arr) => {
